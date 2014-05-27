ba672b4d650562aae34e2cabbe6a4ff0e8193014
HFPDarkAlex <Staralx@mail.ru>
2014-05-27 20:52:02 +0400

    Update AutoCarry Plugin HFP.lua

--- AutoCarry Plugin HFP.lua
+++ AutoCarry Plugin HFP.lua
@@ -4,7 +4,7 @@ local scriptname = "AutoCarry Plugin HFP"
 -----
 local SOURCELIB_URL = "https://raw.github.com/TheRealSource/public/master/common/SourceLib.lua"
 local SOURCELIB_PATH = LIB_PATH.."SourceLib.lua"
-local UPDATE_HOST = "raw.githubusercontent.com"
+local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/HFPDarkAlex/BoL/master/AutoCarry%20Plugin%20HFP.lua"
 local UPDATE_FILE_PATH = SCRIPT_PATH..scriptname..".lua"
 local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
