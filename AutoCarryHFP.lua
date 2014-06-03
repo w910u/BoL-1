30b3f1dc07b6286ab88675d25ec69c1b8ddf6ba0
HFPDarkAlex <Staralx@mail.ru>
2014-06-03 14:17:40 +0400

    Update AutoCarryHFP.lua
    Add new version check and Autoupdater

--- AutoCarryHFP.lua
+++ AutoCarryHFP.lua
@@ -1,15 +1,31 @@
-local version = "0.08"
-local scriptname = "AutoCarry Plugin HFP"
------
-_G.AutoCarry_AutoUpdate = true
------
+--- Info
+local version = 0.09
+local AUTOUPDATE = true
+local SCRIPT_NAME = "AutoCarryHFP"
+--- Update
 local SOURCELIB_URL = "https://raw.github.com/TheRealSource/public/master/common/SourceLib.lua"
 local SOURCELIB_PATH = LIB_PATH.."SourceLib.lua"
-local UPDATE_HOST = "raw.github.com"
-local UPDATE_PATH = "/HFPDarkAlex/BoL/master/AutoCarry%20Plugin%20HFP.lua".."?rand="..math.random(1,10000)
-local UPDATE_FILE_PATH = SCRIPT_PATH..scriptname..".lua"
-local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
------
+
+if FileExist(SOURCELIB_PATH) then
+require("SourceLib")
+else
+DOWNLOADING_SOURCELIB = true
+DownloadFile(SOURCELIB_URL, SOURCELIB_PATH, function() print("Required libraries downloaded successfully, please reload") end)
+end
+
+if DOWNLOADING_SOURCELIB then print("Downloading required libraries, please wait...") return end
+
+if AUTOUPDATE then
+SourceUpdater(SCRIPT_NAME, version, "raw.github.com", "/HFPDarkAlex/BoL/master/"..SCRIPT_NAME..".lua", SCRIPT_PATH .. GetCurrentEnv().FILE_NAME, "/honda7/BoL/master/versions/"..SCRIPT_NAME..".version"):CheckUpdate()
+end
+
+local RequireI = Require("SourceLib")
+RequireI:Add("vPrediction", "https://raw.githubusercontent.com/HFPDarkAlex/Scripts/master/Common/VPrediction.lua")
+RequireI:Add("SOW", "https://raw.githubusercontent.com/HFPDarkAlex/Scripts/master/Common/SOW.lua")
+RequireI:Check()
+
+if RequireI.downloadNeeded == true then return end
+--- libs
 local ignite = nil
 local BRKSlot, DFGSlot, HXGSlot, BWCSlot, TMTSlot, RAHSlot, RNDSlot, SOTDSlot, EntropySlot, YGSlot, HealthSlot, ManaSlot = nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil
 local QREADY, WREADY, EREADY, RREADY, BRKREADY, DFGREADY, HXGREADY, BWCREADY, TMTREADY, RAHREADY, RNDREADY, SOTDREADY, EntropyREADY, YGREADY, HEALTHREADY, MANAREADY = false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false
@@ -20,45 +36,6 @@ local Qrange = 250 --[[ Tumble distance ]]--
 local Erange = 575 --[[ The range of your condemn. Max distance is 450.  ]]--
 local tumblebufferrange = 200 --[[      When an enemy hero enters this range on rambo mode, Vayne will automatically tumble to your mouse.      ]]--
 local tumbleattacknowrange = 250 --[[   After vayne tumbles to your mouse, it will check if enemy hero is 250 distance away. If yes, attack.    ]]--
-
-if FileExist(SOURCELIB_PATH) then
-	require("SourceLib")
-else
-	DOWNLOADING_SOURCELIB = true
-	DownloadFile(SOURCELIB_URL, SOURCELIB_PATH, function() print("Required libraries downloaded successfully, please reload") end)
-end
-
-if DOWNLOADING_SOURCELIB then print("Downloading required libraries, please wait...") return end
-
-function AutoupdaterMsg(msg) print("<font color=\"#FF0000\">"..scriptname..":</font> <font color=\"#FFFFFF\">"..msg..".</font>")
-end
-
-if _G.AutoCarry_AutoUpdate then
-	 local ServerData = GetWebResult(UPDATE_HOST, UPDATE_PATH)
-	if ServerData then
-		local ServerVersion = string.match(ServerData, "local version = \"%d+.%d+\"")
-		ServerVersion = string.match(ServerVersion and ServerVersion or "", "%d+.%d+")
-		if ServerVersion then
-			ServerVersion = tonumber(ServerVersion)
-			if tonumber(version) < ServerVersion then
-				AutoupdaterMsg("New version available: v"..ServerVersion)
-				AutoupdaterMsg("Updating, please don't press F9")
-				DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end)	 
-			else
-				AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
-			end
-		end
-	else
-		AutoupdaterMsg("Error downloading version info")
-	end
-end
-
-local RequireI = Require("SourceLib")
-RequireI:Add("vPrediction", "https://raw.github.com/honda7/BoL/master/Common/VPrediction.lua")
-RequireI:Add("SOW", "https://raw.github.com/honda7/BoL/master/Common/SOW.lua")
-RequireI:Check()
-
-if RequireI.downloadNeeded == true then return end
 
 function OnLoad()
 VP = VPrediction()
